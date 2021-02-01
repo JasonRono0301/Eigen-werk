7ca8fb84d850a81d7cc51a3a8407a3e3d3af44b8
Jason <99057964@mydavinci.nl>
2021-02-01 09:08:56 +0100

    Bezig met de buttons om de counts te verbinden.

--- Mijn werk/Teller /script.js
+++ Mijn werk/Teller /script.js
@@ -1,15 +1,12 @@
-var animailArray = [0,0,0,0];//Array voor de dieren.
-var animals = ['kip','konijn','geit','schaap'];//Array met de dieren namen.
+var animalCount = [0,0,0,0];//Array waarde van de dieren.
+var animals = ['geit','konijn','schaap','koe'];
+console.log(animals);
+console.log(animalCount);
 
 for(i=0; i < animals.length; i++){
     var buttons = document.createElement('button');
-    var text = animals.length;
     document.body.appendChild(buttons);
     buttons.innerHTML = animals[i];
-}
-
-for(i=0; i < animals.length; i++){
-    document.write( "<br>" + animals[i] + ":" + animailArray[i] + "<br>");
 }
 
 
