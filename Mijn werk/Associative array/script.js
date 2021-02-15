8b50856a081a07ce0e4ef14f73e28a137d1dac60
Jason <99057964@mydavinci.nl>
2021-02-15 10:25:56 +0100

    Nog bezig.

--- Mijn werk/Associative array/script.js
+++ Mijn werk/Associative array/script.js
@@ -1,15 +1,17 @@
 //Associative array.
-var drinken = {"fris":0,"bier":0,"wijn":0};
-const PRIJZEN = {"frisPrijs":2,"bierPrijs":3,"wijnPrijs":4};
-    console.log(PRIJZEN);
-    console.log(drinken);
+    var drinken = {"fris":0,"bier":0,"wijn":0};
+    const PRIJZEN = {"frisPrijs":2,"bierPrijs":3,"wijnPrijs":4};
 
-        drinken['fris'] = prompt('Hoeveel fris wilt u bestellen?');
-        drinken['bier'] = prompt('Hoeveel bier wilt u bestellen?');
-        drinken['wijn'] = prompt('Hoeveel wijn wilt u bestellen?');
+        console.log(PRIJZEN);
+        console.log(drinken);
+        
+            drinken['fris'] = prompt('Hoeveel fris wilt u bestellen?');
+            drinken['bier'] = prompt('Hoeveel bier wilt u bestellen?');
+            drinken['wijn'] = prompt('Hoeveel wijn wilt u bestellen?');
 
             var total = {"Fris Totaal": drinken['fris'] * PRIJZEN['frisPrijs'], "Bier Totaal": drinken['bier'] * PRIJZEN['bierPrijs'],"Wijn Totaal": drinken['wijn'] * PRIJZEN['wijnPrijs']};
                 console.log(total);
-for(i in total){
-    document.write(i + "= " + total[i] + " Euro" + "<br>");
-}
+
+            for(i in total){
+                document.write(i + "= " + total[i] + " Euro" + "<br>");
+            }
---
+++ Mijn werk/Digitale Bestelling/script.js
@@ -0,0 +1,24 @@
+//Digitale order.
+
+alert('Welkom bij de digitale order');
+
+var choice = prompt('Welk formaat pizza wilt u bestellen: small of medium of large?');//De keuze prompt.
+
+
+//Variabelen met daarin de hoeveelheden van de pizza.
+var small = 0;
+var medium = 0;
+var large = 0;
+
+//Constanten maar daarin de prijzen van de pizza per formaat.
+const SMALLPRICE = 2;
+const MEDIUMPRICE = 4;
+const LARGEPRICE = 6;
+
+
+
+
+
+
+
+    
